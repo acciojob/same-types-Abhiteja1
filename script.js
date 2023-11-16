7909e6ef795f2ea5e1df1ebcbeb878f6b534629d
Dumpala Abhiteja <125918097+Abhiteja1@users.noreply.github.com>
2023-11-16 21:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 function isSameType(value1, value2) {
-  //your js code here
-	if(typeof value1 === 'string'){
-		value1 = Number(value1);
-	}
-	if(typeof value2 === 'string'){
-		value2 = Number(value2);
-	}
-	if(isNaN(value1)&&isNaN(value2)){
-		return true;
-	}
-	return typeof value1 === typeof value2;
-}
+  // Convert values to numbers if they are strings
+  if (typeof value1 === 'string' && !isNaN(value1)) {
+    value1 = Number(value1);
+  }
+  
+  if (typeof value2 === 'string' && !isNaN(value2)) {
+    value2 = Number(value2);
+  }
 
-// do not change the code below..
+  // Check if types are different and one of them is a string
+  if (typeof value1 !== typeof value2) {
+    return false;
+  }
+
+  // Check if the types are the same
+  return true;
+}
+  
+// Example usage:
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
